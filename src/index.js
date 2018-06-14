a888b9b9075f699b53d431986b2257e1865129f6
Aaron Blackshear <theshiftybapple@gmail.com>
2018-06-14 13:55:20 -0500

    Setting up router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from './ducks/store.js';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
